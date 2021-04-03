a1f30246bcd94a4afe04f8b922ae9801245e61ab
Shehu Abdulkadir <abdulkadirshehu53.sa@gmail.com>
2021-04-03 09:13:28 +0200

    Resolving gh-page issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from "react"
-import LandingPage from "./components/LandingPage/LandingPage";
-import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
-import Login from './components/LoginValidation/Login'
-import Signup from "./components/SignupValidation/Signup"
-import ForgetPassword from "./components/ForgetPassword"
 
-const App = () =>{
+import './App.css';
+
+function App() {
   return (
-    <div>
-      <Router>
-       <Switch>
-        <Route exact component={LandingPage} path='/' />
-        <Route component={Login} path='/login' />
-        <Route component={Signup} path='/signup' />
-        <Route component={ForgetPassword} path='/forgetPassword' />
-  
-       </Switch>
-      </Router>
+    <div className="App">
+      <h1>hello wold</h1>
     </div>
   );
 }
